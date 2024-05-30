a1c23bdec9e8d273b82facbc7ea9790155ed05f3
João Vitor Neves <jvneves_marq@hotmail.com>
2024-05-30 11:59:07 -0300

    fix: remove envSchema

--- src/env.ts
+++ src/env.ts
@@ -1,10 +1,10 @@
-import { z } from 'zod'
+// import { z } from 'zod'
 
 const apiUrl = 'https://sistema-empoderatech.onrender.com/'
 
-const envSchema = z.object({
-  VITE_API_URL: z.string().url(),
-})
+// const envSchema = z.object({
+//   VITE_API_URL: z.string().url(),
+// })
 
 const env = { VITE_API_URL: apiUrl }
 
