1c755ecea1e5b41ba0552904dc08efb0a347155f
João Vitor Neves <jvneves_marq@hotmail.com>
2024-05-30 11:51:28 -0300

    fix: zod and api

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,11 @@
 import { z } from 'zod'
 
+const apiUrl = 'https://sistema-empoderatech.onrender.com/'
+
 const envSchema = z.object({
   VITE_API_URL: z.string().url(),
 })
 
-export const env = envSchema.parse(import.meta.env)
+const env = { VITE_API_URL: apiUrl }
+
+export { env }
--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 import { env } from '../env'
 
+const apiUrl = env.VITE_API_URL
+
 export const api = axios.create({
-  baseURL: env.VITE_API_URL,
+  baseURL: apiUrl,
 })
